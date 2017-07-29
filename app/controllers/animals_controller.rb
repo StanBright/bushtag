65029a0a284eab962041c37a81fa36340e689267
Stan Bright <stan@stanbright.com>
2017-07-29 17:09:17 +1000

    Improve animal#show page

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,6 +1,12 @@
 class AnimalsController < ApplicationController
   def show
-    @name = params[:id]
+    @animal = Animal.where("common_name = ? OR scientific_name = ?", params[:id], params[:id]).first
+    unless @animal
+      flash[:notice] = "Sorry, we couldn't find that animal..."
+      redirect_to root_path and return
+    end
+
+    @name = @animal.scientific_name
 
     @animals_json = Animal.
       select(:scientific_name, :latitude_GDA94, :longitude_GDA94).
@@ -9,6 +15,6 @@ class AnimalsController < ApplicationController
       map { |a| a.map_point }.
       to_json
 
-    @page_title = @name.capitalize
+    @page_title = @name
   end
 end
