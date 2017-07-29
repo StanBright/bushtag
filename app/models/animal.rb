5e167227ca0930a04567da6d01a1561894531cfc
Stan Bright <stan@stanbright.com>
2017-07-29 20:02:55 +1000

    Improve the presentation of data on the homepage

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,21 @@
 class Animal < ApplicationRecord
+  JSON_PATH = "#{Rails.root}/db/site_index.json"
+
+  class << self
+    def export_json
+      json = select(:scientific_name, :latitude_GDA94, :longitude_GDA94, :image_url).
+        all.
+        map { |a| a.map_point }.
+        to_json
+
+      File.write(JSON_PATH, json)
+    end
+
+    def load_json
+      File.read(JSON_PATH)
+    end
+  end
+
   def path
     "/animals/#{scientific_name.gsub(' ', '-')}"
   end
